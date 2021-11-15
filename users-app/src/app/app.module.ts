8c682c96871ff12d916ba0dc396a1057e4e39c07
Sumit K <synergy2411@outlook.com>
2021-11-15 16:45:33 +0530

    bootstrap added

--- users-app/src/app/app.module.ts
+++ users-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/user.component';
 
 @NgModule({
   declarations: [         // Component, Pipe, Directives
-    AppComponent
+    AppComponent, UsersComponent
   ],
   imports: [              // Module - Built-in & Custom
     BrowserModule
---
+++ users-app/src/app/components/users/user.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-users',
+  templateUrl : `./users.component.html`,
+  styleUrls : [`./users.component.css`]
+})
+export class UsersComponent{
+  user = {
+    firstName : "bill",
+    lastName : "gates",
+    income : 50000,
+    dob : new Date('Dec 21, 1964'),
+    isWorking : true,
+    company : "microsoft",
+    imgSrc : "./assets/images/bill.jpg"
+  }
+}
