4dd6981665f3d64417ff626e4592a44a8507498f
Sumit K <synergy2411@outlook.com>
2021-11-18 17:26:59 +0530

    XHR Call

--- expenses/src/Components/practice/UseEffectDemo.js
+++ expenses/src/Components/practice/UseEffectDemo.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const UseEffectDemo = () => {
     const [counter, setCounter] = useState(0)
@@ -10,17 +11,19 @@ const UseEffectDemo = () => {
     // useEffect(() => {
     //     console.log('Use Effect Works')
     // },[])
-    
+
     // useEffect(() => {
     //     console.log('Use Effect Works')
     // },[counter])
 
     useEffect(()=>{
         console.log("Use Effect Works")
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response=>console.log(response)).catch(err=>console.log(err))
         return () => {
             console.log("Clean up Function works")
         }
-    })
+    },[])
 
     return (<div>
         <p>Use effect in action...</p>
