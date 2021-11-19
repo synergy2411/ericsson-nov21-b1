d2c581c33e8f5fc605202b9b21af65f48687355c
Sumit K <synergy2411@outlook.com>
2021-11-19 15:26:12 +0530

    redux devtool added

--- expenses/src/index.js
+++ expenses/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
@@ -10,10 +10,12 @@ import counterReducer from "./store/reducers/counter.reducer";
 import resultReducer from "./store/reducers/result.reducer";
 import logger from "./store/middleware/logger";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(combineReducers({
   ctr : counterReducer,
   res : resultReducer
-}), applyMiddleware(logger));
+}), composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
