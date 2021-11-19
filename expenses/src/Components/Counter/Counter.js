b6bbb5d4324086923dfbfb3c53018dedc59ab557
Sumit K <synergy2411@outlook.com>
2021-11-19 12:59:34 +0530

    actions added

--- expenses/src/Components/Counter/Counter.js
+++ expenses/src/Components/Counter/Counter.js
@@ -1,22 +1,34 @@
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import * as fromActions from '../../store/actions/root-actions';
 
-const Counter = props =>{
-    return (
-        <div>
-            <p className="display-4 text-center">Counter : {props.counter} </p>
+const Counter = (props) => {
+  return (
+    <div>
+      <p className="display-4 text-center">Counter : {props.counter} </p>
+      <div className="text-center">
+        <div className="btn-group">
+          <button onClick={props.onIncrement} className="btn btn-primary">
+            Increase
+          </button>
+          <button className="btn btn-warning" onClick={props.onDecrement}>Decrease</button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-        counter : state.counter
-    }
-}
+  return {
+    counter: state.counter,
+  };
+};
 const mapDispatchToProps = (dispatch) => {
-    return {
-        onIncrement : () => console.log("On Increment")
-    }
-}
+  return {
+    onIncrement: () => dispatch({ type: fromActions.INCREMENT }),
+    onDecrement: () => dispatch({ type : fromActions.DECREMENT })
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+const WrapperFn = connect(mapStateToProps, mapDispatchToProps);
+
+export default WrapperFn(Counter);
--- expenses/src/store/reducers/root-reducer.js
+++ expenses/src/store/reducers/root-reducer.js
@@ -1,9 +1,24 @@
+import * as fromActions from '../actions/root-actions';
+
 const initialState = {
     counter : 0
 }
 
 function rootReducer(state = initialState, action){
-    return state;
+    switch (action.type) {
+        case fromActions.INCREMENT:{
+            return {
+                counter : state.counter + 1
+            }
+        }
+        case fromActions.DECREMENT:{
+            return {
+                counter : state.counter - 1
+            }
+        }
+        default:
+            return state;
+    }
 }
 
 export default rootReducer;
